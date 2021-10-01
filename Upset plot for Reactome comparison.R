47ff154de5ec6f520b1007256eaebae5a59c8ef8
Sam Old <40485627+SameOldSamOld@users.noreply.github.com>
2021-10-01 15:52:18 +1300

    Add files via upload

---
+++ Upset plot for Reactome comparison.R
@@ -0,0 +1,13 @@
+library(UpSetR)
+input <- read.csv("https://raw.githubusercontent.com/SameOldSamOld/RNA-Seq_OG4953/master/REACTOME_COMPARISON_LIST.csv",
+                  row.names = 1)
+
+upset(input,
+      order.by = c("degree"),
+      mb.ratio = c(0.6, 0.4),
+      number.angles = 0,
+      text.scale = 1.1,
+      point.size = 2.8,
+      line.size = 1
+      )
+
